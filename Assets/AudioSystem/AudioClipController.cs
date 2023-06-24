8101c5d01b9f4ecb0c0a76bdf91ee6c87713a1d8
Alan <alan.williams.brito@gmail.com>
2023-06-23 22:37:44 -0400

    Added functional Audio System

---
+++ Assets/AudioSystem/AudioClipController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "AudioSystem/AudioClipController", fileName = "AudioClipController")]
+public class AudioClipController : ScriptableObject
+{
+	public string clipName;
+	public AudioClip audioClip;
+	public bool loopClip;
+
+	[SerializeField]
+	AudioChannel audioChannel;
+
+	public void Play()
+	{
+		audioChannel?.PlayClip(this);
+	}
+
+	public void Play(AudioChannel audioChannel)
+	{
+		audioChannel.PlayClip(this);
+	}
+}
