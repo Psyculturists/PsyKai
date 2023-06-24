2d3bfc054949325f27ac1752a3126759129bb6f4
Alan <alan.williams.brito@gmail.com>
2023-06-24 19:01:32 -0400

    Added Void Spawn Mechanic

---
+++ Assets/Scripts/System/SceneController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "Systems/SceneController", fileName = "SceneController")]
+public class SceneController : ScriptableObject
+{
+	public void ReloadCurrentScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
